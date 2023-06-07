c76e7d40eb046105dd9015c94309af1c088d968b
youssefjn <youssefjneyah@gmail.com>
2023-06-07 11:10:41 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import talenddemo.howtosetupjoinlink_0_1.HowToSetupJoinLink;
+
+public class Main {
+public static void main(String[] args) {
+	try {
+		HowToSetupJoinLink howToSetupJoinLink = new HowToSetupJoinLink();
+		String [] context = new String [] {};
+		howToSetupJoinLink.runJob(context);
+		System.out.println("done");
+		} catch (Exception e) {
+e.printStackTrace();
+}
+}}
